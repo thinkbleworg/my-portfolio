224f6b8f42d501bd109c1624c2750ae2567f9aef
ALTIMETRIK\amartin12136 <amartin12136@altimetrik.com>
2025-04-08 14:00:26 +0530

    Logs

--- server/api/contact.js
+++ server/api/contact.js
@@ -1,40 +1,43 @@
-
 import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
-    if (req.method !== "POST") {
-        return res.status(405).json({ message: "Method Not Allowed" });
-      }
-    
-      const { name, email, message } = req.body;
-    
-      if (!name || !email || !message) {
-        return res.status(400).json({ message: "All fields are required" });
-      }
-    
-      try {
-        const transporter = nodemailer.createTransport({
-          service: "yahoo",
-          auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-          },
-        });
-    
-        const mailOptions = {
-          from: process.env.EMAIL_USER,
-          to: process.env.RECEIVER_EMAIL,
-          subject: `New Contact Form Submission from ${name}`,
-          text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-        };
-    
-        await transporter.sendMail(mailOptions);
-        return res.status(200).json({ message: "Email sent successfully" });
-      } catch (error) {
-        console.error("Error sending email:", error);
-        return res
-          .status(500)
-          .json({ message: "Failed to send email", error: error.toString() });
-      }
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
 
-};
+  console.log("EMAIL_USER:", process.env.EMAIL_USER);
+  console.log("RECEIVER_EMAIL:", process.env.RECEIVER_EMAIL);
+
+  const { name, email, message } = req.body;
+
+  if (!name || !email || !message) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "yahoo",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: process.env.RECEIVER_EMAIL,
+      subject: `New Contact Form Submission from ${name}`,
+      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    };
+
+    console.log("Receiver Email:", process.env.RECEIVER_EMAIL);
+
+    await transporter.sendMail(mailOptions);
+    return res.status(200).json({ message: "Email sent successfully" });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return res
+      .status(500)
+      .json({ message: "Failed to send email", error: error.toString() });
+  }
+}
