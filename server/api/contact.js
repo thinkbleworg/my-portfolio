9f23f73c98129245ac90586dec23f2bb5983f917
Think Blew <thinkbleworg@gmail.com>
2025-04-08 19:11:08 +0530

    File changes

--- server/api/contact.js
+++ server/api/contact.js
@@ -3,4 +3,4 @@ import serverless from "serverless-http";
 import app from "../expressApp.js";
 
 // Export the serverless handler
-export default serverless(app);
+export const handler = serverless(app);
